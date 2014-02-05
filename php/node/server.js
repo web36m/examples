e87714aadd038e966a9e7c37ed8609b5ef3906aa
Shilov Vasily <web36m@yandex.ru>
2014-02-05 08:25:22 +0300

    Create server.js

---
+++ php/node/server.js
@@ -0,0 +1,19 @@
+var express = require('express'),
+http = require('http'),
+path = require('path'),
+phpExpress = require('php-express')({
+  binPath: '/usr/bin/php'
+});
+
+var app = express();
+app.set('port', process.env.PORT || 7345);
+app.use(express.bodyParser());
+
+app.set('views', path.join(__dirname, 'dev'));
+app.engine('php', phpExpress.engine);
+app.set('view engine', 'php');
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.all(/.+\.php$/, phpExpress.router);
+http.createServer(app).listen(app.get('port'));
